a6c01ee95e13d8bf49b7fd14305eea14506ba552
Patrick Palmer <patrickapalmer@gmail.com>
2025-03-03 06:34:56 -0500

    remove no longer needed files

--- UnrealLiveLinkCInterface/BuildUnrealLiveLinkCInterface.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-RMDIR %~dp0Staging /S /Q
-..\..\..\Build\BatchFiles\RunUAT.bat  BuildGraph -Script=Engine/Source/Programs/UnrealLiveLinkCInterface/BuildUnrealLiveLinkCInterface.xml -Target="Live Link CInterface Module"
